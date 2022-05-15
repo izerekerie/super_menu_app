ce6ce91d1650dd232b650934ae9366ace776ea21
izereuwonkunda <izereuwonkundakelia@gmail.com>
2022-05-15 14:44:51 +0200

    navigation

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text ,SafeAreaView} from 'react-native'
 
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={{flex:1}} >
+      <Text>App</Text>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ screens/Menu.js
@@ -0,0 +1,13 @@
+import { View, Text,SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <SafeAreaView style={{flex:1}}>
+        
+      <Text>Menu</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Menu
