2b1eaeb9b457ac8f512770c4e55d2c732a25f352
pauline12ish34 <paulineishimwe12@gmail.com>
2022-05-15 19:09:19 +0200

    Authentication

--- App.js
+++ App.js
@@ -1,24 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Navigation from './navigation';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from 'react'
+import SignUp from './screens/SignUp'
 
-export default function App() {
+const App = () => {
   return (
-    <SafeAreaProvider >
-      <Navigation/>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      <StatusBar style="auto" />
-    </SafeAreaProvider>
-    
-  );
+   <View>
+<SignUp/>
+
+   </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// // import Navigation from './navigation';
+// import { SafeAreaProvider } from 'react-native-safe-area-context';
+// import Login from './screens/Login';
+// import SignUp from './screens/SignUp';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// const stack= createStackNavigator();
+
+//  function App() {
+//   return (
+//   < Stack.Navigator screenOptions={{headerShown:false}}>
+//   <Stack.screen name="login" component={Login}/>
+//   <Stack.screen name="signUp" component={SignUp}/>
+//   </Stack.Navigator>
+    
+//   );
+// }
+
+
+// export default  () => {
+//   return (
+//     <NavigationContainer>
+//       <App/>
+//       </NavigationContainer>
+//   )
+// }
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
